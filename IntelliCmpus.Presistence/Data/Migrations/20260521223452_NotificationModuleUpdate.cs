b8d5cf92541e787ebe1fbbca5a911433f4b286cb
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2026-05-22 01:49:55 +0300

    Add Notification Module Migrations

---
+++ IntelliCmpus.Presistence/Data/Migrations/20260521223452_NotificationModuleUpdate.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntelliCampus.Presistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationModuleUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Posts_PostId",
+                table: "Notifications");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserNotifications",
+                table: "UserNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notifications_PostId",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserNotificationId",
+                table: "UserNotifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "UserNotifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Notifications",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserNotifications",
+                table: "UserNotifications",
+                column: "UserNotificationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_UserId",
+                table: "UserNotifications",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserNotifications",
+                table: "UserNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserNotifications_UserId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "UserNotificationId",
+                table: "UserNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "UserNotifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Notifications",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Notifications",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserNotifications",
+                table: "UserNotifications",
+                columns: new[] { "UserId", "NotificationId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_PostId",
+                table: "Notifications",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Posts_PostId",
+                table: "Notifications",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "PostId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
