99b319f6aa5205555b466eabee36ca90aad1e8d3
Mohamed Adel <111653519+MuhammedxAdell@users.noreply.github.com>
2026-06-24 18:48:40 +0300

    Add Json IgnoDocument in Submit Excuse

--- IntelliCampus.shared/Dtos/Attendance/SubmitExcuseFormDto.cs
+++ IntelliCampus.shared/Dtos/Attendance/SubmitExcuseFormDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http;
 
 namespace IntelliCampus.shared.Dtos.Attendance;
@@ -10,5 +11,6 @@ public class SubmitExcuseFormDto
 
     public string? Reason { get; set; }
 
+    [JsonIgnore]
     public IFormFile? Document { get; set; }
 }
