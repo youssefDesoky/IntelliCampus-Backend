7d32e7f95ceb364f215ebeaf6587555eb6ba9a87
mazenkhaled98 <mazenkhaled8@gmail.com>
2026-06-25 22:00:43 +0300

    edit quiz

--- IntelliCampus.Service/QuizService.cs
+++ IntelliCampus.Service/QuizService.cs
@@ -285,13 +285,14 @@ public class QuizService : IQuizService
         if (!teachesCourse)
             throw new InvalidOperationException("Not authorized.");
 
-        var existingQuestions = await QuestionsRepo.GetAllAsync(new QuestionsByQuizSpec(quizId));
-        var existingPoints = existingQuestions.Sum(q => q.Points);
         var newPoints = questions.Sum(q => q.Points);
-        var totalAfterAdd = existingPoints + newPoints;
 
-        if (totalAfterAdd != quiz.MaxGrade)
-            throw new InvalidOperationException($"Question points must equal the quiz max grade of {quiz.MaxGrade}. Current total: {existingPoints}, new total would be: {totalAfterAdd}.");
+        if (newPoints != quiz.MaxGrade)
+        {
+            var diff = newPoints - quiz.MaxGrade;
+            var direction = diff > 0 ? "more" : "less";
+            throw new InvalidOperationException($"Quiz max grade is {quiz.MaxGrade} but questions total {newPoints} ({Math.Abs(diff)} points {direction}).");
+        }
 
         foreach (var q in questions)
         {
