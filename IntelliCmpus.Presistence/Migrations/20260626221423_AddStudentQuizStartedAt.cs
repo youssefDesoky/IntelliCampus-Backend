4bc4f4ab3ebbe5543d7e3c1317da0f92b2a07fb3
Youssef Ahmed <147060769+yousseffahmeddd@users.noreply.github.com>
2026-06-27 01:22:46 +0300

    fixing migrations

---
+++ IntelliCmpus.Presistence/Migrations/20260626221423_AddStudentQuizStartedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntelliCampus.Presistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentQuizStartedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartedAt",
+                table: "StudentQuizzes",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartedAt",
+                table: "StudentQuizzes");
+        }
+    }
+}
